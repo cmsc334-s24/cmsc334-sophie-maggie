53da9d84efbf6d046cb805e94fe0bd0ae65b9b4e
KexuanZhao <98555862+KexuanZhao@users.noreply.github.com>
2024-04-21 20:55:20 -0400

    add passwd check related files

---
+++ get_data.py
@@ -0,0 +1,9 @@
+import requests
+def download_dataset(url, file_path):
+    response = requests.get(url)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+dataset_url = 'https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.txt'
+dataset_file_path = 'passwords.txt'
+download_dataset(dataset_url, dataset_file_path)
