7cb5e4e2899c31c02c83e8db8c79e10b5d07ee9a
KexuanZhao <sophiezhaokx@gmail.com>
2024-05-01 20:31:33 -0400

    Upload all the code

--- get_data.py
+++ get_data.py
@@ -1,4 +1,6 @@
+# Get the password dataset from the webpage
 import requests
+
 def download_dataset(url, file_path):
     response = requests.get(url)
     with open(file_path, 'wb') as file:
--- main.py
+++ main.py
@@ -1,55 +1,43 @@
-import os
+# CMSC 334: Computer Security Final Project
+# @authors: Sophie Zhao & Maggie Song
 import hashlib
+from passwd_check import load_passwords_from_txt, check_password
 from text_ende import create_entry, append_entry, read_entry
-from image_ende import image_encrypt, image_decrypt
 from pdf_ende import pdf_encrypt, pdf_decrypt
-
-def load_passwords_from_txt(file_path):
-    with open(file_path, 'r', encoding = 'utf-8') as file:
-        passwords = file.read().splitlines()
-    return passwords
-
-
-def check_password(user_input, passwords):
-    return user_input in passwords
-
+from multimedia_ende import multimedia_encrypt, multimedia_decrypt
 
 def main():
     # Load passwords
     passwords = load_passwords_from_txt('passwords.txt')
 
+    # Prompt user to enter password
     while True:
         user_input = input("Enter your password: ")
+        if len(user_input) <= 4:
+            print("Password is too short. The length of the password should be greater than 4.")
 
         if check_password(user_input, passwords):
             print("Password is commonly used. Please choose a stronger password.")
         else:
-            print("Password is not commonly used. Good to go!")
+            print("Password is not commonly used. Good to go! \n")
             break
-
+    
+    # Derive the key for the text encryption & decryption from the user typed password
     salt = b'totalrandomsalt' 
     key = hashlib.pbkdf2_hmac('sha256', user_input.encode(), salt, 100000)  
     key = key[:16]
-
+    
     print("Welcome to the File Management System!")
-
-    # Check if the key file exists
-    if os.path.exists('key.bin'):
-        # Read the key from the file
-        with open('key.bin', 'rb') as key_file:
-            key = key_file.read()
 
     # While loop for operations
     while True:
         print("")
         print("What type of file would you like to manage?")
         print("1. Text file")
-        print("2. Image file")
-        print("3. PDF file")
-        print("4. Audio file")
-        print("5. Exit")
-        filetype = input("Enter the number of the file type you would like to manage:")
-        
+        print("2. PDF file")
+        print("3. Image/Audio/Video file")
+        print("4. Exit")
+        filetype = input("Enter the number of the file type you would like to manage: ")
         if filetype == "1":
             print("\nChoose an operation:")
             print("(x) : Return to main menu")
@@ -57,6 +45,9 @@ def main():
             print("(r) : Read a text file")
             print("(a) : Append to an existing text file")
             choice = input()
+            while choice not in ['x', 'n', 'r', 'a']:
+                print("ERROR: Invalid input. Choose again.")
+                choice = input()
             if choice == 'x':
                 continue
             elif choice == 'n':
@@ -65,55 +56,37 @@ def main():
                 read_entry(key)
             elif choice == 'a':
                 append_entry(key)
-            else:
-                print("ERROR: Invalid input. Choose again.")
-        
         elif filetype == "2":
-            print("\nChoose an operation:")
-            print("(x) : Return to main menu")
-            print("(e) : Encrypt an image file")
-            print("(d) : Decrypt an image file")
-            choice = input()
-            if choice == 'x':
-                continue
-            elif choice == 'e':
-                image_encrypt(key)
-            elif choice == 'd':
-                image_decrypt(key)
-            else:
-                print("ERROR: Invalid input. Choose again.")
-       
-        elif filetype == "3":
             print("\nChoose an operation:")
             print("(x) : Return to main menu")
             print("(e) : Encrypt a PDF file")
             print("(d) : Decrypt a PDF file")
             choice = input()
+            while choice not in ['x', 'e', 'd']:
+                print("ERROR: Invalid input. Choose again.")
+                choice = input()
             if choice == 'x':
                 continue
             elif choice == 'e':
                 pdf_encrypt(user_input)
             elif choice == 'd':
                 pdf_decrypt(user_input)
-            else:
-                print("ERROR: Invalid input. Choose again.")
-        
-        elif filetype == "4":
+        elif filetype == "3":
             print("\nChoose an operation:")
             print("(x) : Return to main menu")
-            print("(e) : Encrypt an audio file")
-            print("(d) : Decrypt an audio file")
+            print("(e) : Encrypt an image/audio/video file")
+            print("(d) : Decrypt an image/audio/video file")
             choice = input()
+            while choice not in ['x', 'e', 'd']:
+                print("ERROR: Invalid input. Choose again.")
+                choice = input()
             if choice == 'x':
                 continue
             elif choice == 'e':
-                print("Audio file management is not supported in this version.")
+                multimedia_encrypt(user_input)
             elif choice == 'd':
-                print("Audio file management is not supported in this version.")
-            else:
-                print("Audio file management is not supported in this version.")
-        
-        elif filetype == "5":
+                multimedia_decrypt(user_input)
+        elif filetype == "4":
             print("Exiting the File Management System. Goodbye!")
             break
 
